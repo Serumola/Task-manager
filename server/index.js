8d22fa97a17205d017433e1a689ccd6dd9c95771
Serumola <pakoserumola02@gmail.com>
2026-04-27 17:00:34 +0200

    i am trying

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS Configuration - Allow specific production origins
 const allowedOrigins = [
-  'https://task-manager-02e.netlify.app',
+  'https://task-manager-02e.netlify.app/',
   'http://localhost:5173',
   'http://localhost:3000'
 ];
